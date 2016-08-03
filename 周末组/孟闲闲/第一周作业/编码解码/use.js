b2d67ed041958ea4688589688a9da6937ab73ca1
webmaxxSELF <webmxxself@163.com>
2016-08-03 18:20:49 +0800

    encording

---
+++ 周末组/孟闲闲/第一周作业/编码解码/use.js
@@ -0,0 +1,9 @@
+var encoding = require('./recoding.js');
+var decofing = require('./decoding.js')
+
+
+// 编码
+console.log('逐 的编码结果是 '+ encoding('逐'));
+
+// 解码
+console.log(  '6YCQ 的解码结果是   '  + decofing('6YCQ'));
