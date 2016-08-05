4fbb6e470ef02670a46c682b99fec2d84f3f11a4
webmaxxSELF <webmxxself@163.com>
2016-08-05 15:56:24 +0800

    buffer

--- 周末组/孟闲闲/第一周作业/fsDirs.js
+++ 周末组/孟闲闲/第一周作业/fsDirs.js
@@ -6,13 +6,12 @@ function mkdir(obj){
         partPath.push(item);
         dirPath = partPath.join('/');
         (function (dirPath) {
-            console.log(dirPath);
             fs.exists(dirPath, function (exists) {
                 if(!exists){
                     fs.mkdir(dirPath, function (err) {
                         if(!err){
                             console.log(dirPath+' done!');
-                            mkdir(obj); //在去掉这行之后无法正确创建
+                            mkdir(obj);
                         }
                     })
                 }
